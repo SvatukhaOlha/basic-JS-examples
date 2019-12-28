b06174d19aab18bd69c8cdf653504180b2cbbafe
Olha Svatukha <57961694+SvatukhaOlha@users.noreply.github.com>
2019-12-28 18:20:17 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,7 +1,23 @@
-1. Next year will be:
+/*1. Next year will be:*/
 const year = 2019;
 alert(year);
 const nextYear = year + 1;
 
 message = 'Next Year will be:' + nextYear;
 alert(message);
+
+/* 2.Next age will be:*/
+const age = parseFloat(prompt('Enter your age'));
+const nextAge = addOne(age);
+alert('In next year your age will be ' + nextAge);
+
+if (age < 18) {
+    alert('You are not adult!');
+} else {
+    alert('You are adult!');
+}
+
+function addOne(num) {
+    const result = num + 1;
+    return result; 
+}
