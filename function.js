7a04df2bd07ceb76d437b7d52baf92a72ef59dbb
Olha Svatukha <57961694+SvatukhaOlha@users.noreply.github.com>
2019-12-28 19:10:33 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,4 @@
-/*1. Next year will be:*/
+//1. Next year will be:
 const year = 2019;
 alert(year);
 const nextYear = year + 1;
@@ -6,7 +6,7 @@ const nextYear = year + 1;
 message = 'Next Year will be:' + nextYear;
 alert(message);
 
-/* 2.Next age will be:*/
+//2.Next age will be:
 const age = parseFloat(prompt('Enter your age'));
 const nextAge = addOne(age);
 alert('In next year your age will be ' + nextAge);
@@ -21,3 +21,24 @@ function addOne(num) {
     const result = num + 1;
     return result; 
 }
+
+//3. How many seconds(and minutes) in an hour, day, year. How many seconds in your age
+const secondsInAMinute = 60;
+const minutesInAHour = 60;
+secondsInAHour = secondsInAMinute * minutesInAHour;
+console.log(secondsInAHour);
+
+const hoursInADay = 24;
+secondsInADay = secondsInAHour * hoursInADay;
+console.log(secondsInADay);
+
+minutesInADay = minutesInAHour * hoursInADay;
+console.log(minutesInADay);
+
+const daysInAYear = 365;
+secondInAYear = daysInAYear * secondsInADay;
+console.log(secondInAYear);
+
+let age = 24;
+ageSeconds = 24 * secondInAYear;
+
