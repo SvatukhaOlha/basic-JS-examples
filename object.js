fbb399884a549f43353bf73d78d96532ed687e45
Olha Svatukha <57961694+SvatukhaOlha@users.noreply.github.com>
2019-12-30 20:30:49 +0200

    Update object.js

--- object.js
+++ object.js
@@ -1,2 +1,14 @@
 //1. delete - delete properties from objects
 delete myObj.prop;
+
+//2. add new properties to obj
+myObj.prop = "this is new prop";
+
+//3 .hasOwnProperty() - to check if the property of a given object exists or not
+.hasOwnProperty(propname)
+let myObj = {
+  top: "hat",
+  bottom: "pants"
+};
+myObj.hasOwnProperty("top");    // true
+myObj.hasOwnProperty("middle"); // false
